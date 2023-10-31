3b8da6298318fba457957fc8ab8152d450e7c5b7
Renan Alexandre <renan.berton@cportal.com.br>
2023-10-31 15:40:52 -0300

    Feature: Criada uma rota para não dar erro no projeto, ao acessar a URL raiz

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/',[PokemonController::class, 'getAllPokemons'])->name('index');
 Route::get('/index/{offset?}',[PokemonController::class, 'getAllPokemons'])->name('index');
 Route::get('/search-pokemon',[PokemonController::class, 'searchPokemon'])->name('searchPokemon');
 Route::get('/pokemon-not-found',[PokemonController::class, 'pokemon-not-found'])->name('pokemon-not-found');
