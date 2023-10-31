d2006eac111fa04269d9ed87736c6f5678092754
Renan Alexandre <renan.berton@cportal.com.br>
2023-10-31 14:34:13 -0300

    Modify: Alteração do Spin de Carregamento + Ajustes CSS

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pokédex - Laravel</title>
+    <title>Pokédex</title>
     <link rel="stylesheet" href="./css/index.css">
     <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 </head>
@@ -35,7 +35,6 @@
         </ul>
         <button id="loadMore">
             <img src="/images/pokeball.png" width="40px" alt="Ícone da Pokebola">
-            Carregar Mais
         </button>
         <div id="loading" style="display: none;">
             <img src="/images/loading-animation.gif" style="background-color: transparent !important" alt="Carregando..." width="50px">
