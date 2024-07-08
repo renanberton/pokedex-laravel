0f5766ffbaa00dd56c5426b3eec409eb758cdaf8
renanberton <renan_berton@yahoo.com.br>
2024-07-08 20:46:56 -0300

    teste 5

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,4 @@ Route::get('/',[PokemonController::class, 'getAllPokemons'])->name('index');
 Route::get('/index/{offset?}',[PokemonController::class, 'getAllPokemons'])->name('index');
 Route::get('/search-pokemon',[PokemonController::class, 'searchPokemon'])->name('searchPokemon');
 Route::get('/pokemon-not-found',[PokemonController::class, 'pokemon-not-found'])->name('pokemon-not-found');
+/* Teste */
